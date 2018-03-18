9e2facfe6b1851003b72d61103824c14e76a1e5a
LobboAngelov <l.angelov96@gmail.com>
2018-03-18 22:29:51 +0200

    fixing smazan.png

--- Tupkach.Bot.NetStandart/Program.cs
+++ Tupkach.Bot.NetStandart/Program.cs
@@ -13,19 +13,17 @@ namespace Tupkach.Bot.NetStandart
         public static void Main(string[] args)
         {
             new Program().MainAsync(args).GetAwaiter().GetResult();
-        }   
-        
+        }
+
         public async Task MainAsync(string[] args)
         {
             using (IKernel kernel = new StandardKernel(new TupkachKernel()))
-            {
-                StaticData.LifetimeScope = kernel;
+            {                
                 kernel.Load(Assembly.GetExecutingAssembly());
                 var bot = kernel.Get<IClientBot>();
                 var handler = kernel.Get<ICommandHandler>();
                 handler.Execute();
-                await bot.StartAsync();
-                await bot.LogAsync("Kur za neshtastieto");
+                await bot.StartAsync();                
                 await Task.Delay(-1);
             }
         }
